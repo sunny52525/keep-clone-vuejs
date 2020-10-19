6c13342280cf9981e78f1c2d3f3250cfaf57c267
Sunny <30768018+sunny52525@users.noreply.github.com>
2020-10-19 13:53:15 +0530

    updated readme

--- src/main.js
+++ src/main.js
@@ -4,12 +4,10 @@ import vuetify from './plugins/vuetify';
 import VuePackeryPlugin from 'vue-packery-plugin'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@mdi/font/css/materialdesignicons.css'
-import VueDraggabillyPlugin from 'vue-packery-draggabilly-plugin'
 Vue.config.productionTip = false
 
 export const eventBus=new Vue();
 Vue.use(VuePackeryPlugin);
-Vue.use(VueDraggabillyPlugin);
 new Vue({
   vuetify,
   render: h => h(App)
